cb52a6c6ec48caf0a1fdf20b755a6748d29110ca
simransethi643 <71235263+simransethi643@users.noreply.github.com>
2021-01-30 14:56:39 -0500

    Update README.py

--- README.py
+++ README.py
@@ -1,3 +1,3 @@
 # know-the-news-around-you
-<button type="button">Topic 1</button>;
-<button type="button">Topic 2</button>;
+<button type="button">Topic 1</button>
+<button type="button">Topic 2</button>
