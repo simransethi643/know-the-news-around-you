eb39f9e145921072c48a678c38fc3b53f4572f7c
simransethi643 <71235263+simransethi643@users.noreply.github.com>
2021-01-31 01:30:33 -0500

    Update README.py

--- README.py
+++ README.py
@@ -1,5 +1,26 @@
-# know-the-news-around-you
-<button type="button">Topic 1</button>
-<button type="button">Topic 2</button>
-a = "How do you feel today?"
-print(a)
+<b style="color:black">Know the news all around you</b>
+<p style="color:black">How are you feelig today?</p>
+<html>
+<head>
+ <meta charset="utf-8" />
+ <title>Know the news around you</title>
+ <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+<body>
+ <button id='btn1' type="button" onclick="function1()">News 1</button>
+ <button id='btn2' type="button" onclick="function2()">News 2</button>
+ <button id='btn3' type="button" onclick="function3()">News 3</button>
+ <button id='btn3' type="button" onclick="function3()">Links to sources</button>
+ <script>
+ function function1() {
+ ;
+ }
+ function function2() {
+ ;
+ }
+ function function3() {
+;
+ }
+ </script>
+</body>
+</html>
